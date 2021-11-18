e98c371bc54ba9712c1b8e704fc78e3eb7382d94
George <mazzeogeorge@gmail.com>
2021-11-18 16:53:08 -0500

    gonna be in git hell for a sec

---
+++ main.py
@@ -0,0 +1,72 @@
+import os
+import requests
+import polling
+import json
+from pydub import AudioSegment
+from pydub.playback import play
+from tempfile import NamedTemporaryFile
+from requests.auth import HTTPBasicAuth
+
+
+class UberDuck():
+    def __init__(self, key, secret):
+        self.key = key
+        self.secret = secret
+
+    def get_voice(self, voice_name, speech):
+        data = {"speech": speech, "voice": voice_name}
+        data_json = json.dumps(data)
+        # Making a get request
+        response = requests.post('https://api.uberduck.ai/speak',
+                                 auth=(self.key, self.secret),
+                                 data=data_json)
+        if response.status_code == 200:
+            print("POLLING")
+        else:
+            print('Unsuccessful request')
+            return -1
+
+        try:
+            uuid = response.json()['uuid']
+        except:
+            if response.json()['detail']:
+                print('ERROR')
+                print(response.json()['detail'])
+                return -1
+
+        def test(response):
+            print(response.json())
+            if response.json()['path'] == 'null' or response.json()['path'] is None:
+                return False
+            else:
+                return True
+            # return response.status_code == 200
+
+        # polls every second times out after 1 min
+        result = polling.poll(lambda: requests.get(f'https://api.uberduck.ai/speak-status?uuid={uuid}',
+                                                   auth=(self.key, self.secret)),
+                              timeout=60,
+                              step=1,
+                              check_success=test)
+        print('GOT RESULT')
+        #self.last_result = result
+        out = requests.get(result.json()["path"])
+        return out.content
+
+    def play_voice(self, raw_sound_bytes):
+        with NamedTemporaryFile(delete=False) as f:
+            f.write(raw_sound_bytes)
+            # for playing wav file
+            song = AudioSegment.from_wav(f.name)
+            print('playing sound using pydub')
+            play(song)
+
+    def download_result(self, raw_sound_bytes, file_name):
+        with open(file_name, 'wb+') as output_file:
+            output_file.write(raw_sound_bytes)
+
+
+# my_duck = UberDuck(os.environ['UBERDUCK_Key'], os.environ['UBERDUCK_Secret'])
+# eminem_im_back = my_duck.get_voice('eminem', "I'm back")
+# my_duck.play_voice(eminem_im_back)
+# my_duck.download_result(eminem_im_back, 'eminem_audio.wav')
